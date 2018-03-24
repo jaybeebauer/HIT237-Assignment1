6812e41df7f5413fedeecf0410d049d0901355ab
Joshua Bauer <joshybee@gmail.com>
2018-03-24 11:47:12 +0800

    -Initial, with Django migrations
    Project setup

---
+++ HIT237-Assignment1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Joshua.Bauer\source\repos\HIT237-Assignment1\HIT237-Assignment1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
