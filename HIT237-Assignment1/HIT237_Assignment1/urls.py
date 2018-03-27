20ae2179045a28d8036d2c0449bd820f702883ab
Joshua Bauer <joshybee@gmail.com>
2018-03-27 21:30:44 +0800

    -Added book list function and page

--- HIT237-Assignment1/HIT237_Assignment1/urls.py
+++ HIT237-Assignment1/HIT237_Assignment1/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', views.home, name='home'),
+    url(r'^books$', views.books, name='books'),
 ]
--- HIT237-Assignment1/books_repo/views.py
+++ HIT237-Assignment1/books_repo/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def books(request):
+    page_data = {'book_list': get_books() }
+    return render(request, 'books.html', page_data)
+
+class Book:
+    def __init__(self, id, title, isbn, author):
+        self.id = id
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+
+def get_books():
+    booklist = []
+    booklist.append(Book(1,'Book1','15123123123','Mr Author Page'))
+    booklist.append(Book(2,'Book2','52342342349','Mr Page File'))
+    return booklist
