befbf9f8b8182abb09de8e86eed2106d8f96403f
Joshua Bauer <joshybee@gmail.com>
2018-04-05 19:06:50 +0800

    -Created seperate file for books class

---
+++ HIT237-Assignment1/books_repo/book.py
@@ -0,0 +1,11 @@
+class book(object):
+    #Define a book object
+    def __init__(self, id, title, isbn, author, genre, publisher, summary):
+        self.id = id
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+        self.genre = genre
+        self.publisher = publisher
+        self.summary = summary
+
--- HIT237-Assignment1/books_repo/views.py
+++ HIT237-Assignment1/books_repo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from books_repo.book import book
 
 # Create your views here.
 def home(request):
@@ -21,18 +22,8 @@ def single_book(request, id):
     page_data = {'book': book }
     return render(request, 'book.html', page_data)
 
-class Book:
-    def __init__(self, id, title, isbn, author, genre, publisher, summary):
-        self.id = id
-        self.title = title
-        self.isbn = isbn
-        self.author = author
-        self.genre = genre
-        self.publisher = publisher
-        self.summary = summary
-
 def get_books():
     booklist = []
-    booklist.append(Book(1,'Book1',15123123123,'Mr Author Page','space','Puffin','The book is about, etc'))
-    booklist.append(Book(2,'Book2',52342342349,'Mr Page File','space','Puffin','The book is about, etc'))
+    booklist.append(book(1,'Book1',15123123123,'Mr Author Page','space','Puffin','The book is about, etc'))
+    booklist.append(book(2,'Book2',52342342349,'Mr Page File','space','Puffin','The book is about, etc'))
     return booklist
